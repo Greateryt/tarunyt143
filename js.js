744d5817bcb66b8717396177913e278035a23b57
greateryt <117499960+Greateryt@users.noreply.github.com>
2023-10-29 17:17:53 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+function run1()
+{
+    $(".nav-item").click(function(){
+        $("#Navbar").slideUp();
+    });
+}
+function run2()
+{
+    $(".navbar-toggler").click(function(){
+        $("#Navbar").slideDown();
+    });
+}
+
